8e6099a8fba2630fcae89c0a4a16dba482210a90
higashionna <hihigashi001@gmial.com>
2022-02-07 22:57:29 +0900

    色変更

--- src/components/SharedScrollToTop.tsx
+++ src/components/SharedScrollToTop.tsx
@@ -12,7 +12,7 @@ export const SharedScrollToTop = () => {
   };
 
   const topButtonClass =
-    "fixed bottom-10 right-6 rounded-full h-16 w-16 z-50 bg-green-800 hover:bg-green-500 focus:outline-none text-white font-bold";
+    "fixed bottom-10 right-6 rounded-full h-16 w-16 z-50 bg-gray-700 hover:bg-gray-500 focus:outline-none text-white font-bold";
 
   return (
     <button className={topButtonClass} onClick={scrollToTop}>
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import { useTable, useSortBy } from 'react-table'
+import React from "react";
+import { useTable, useSortBy } from "react-table";
 
 export const Table = ({ columns, data }) => {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
-    columns,
-    data,
-  }, useSortBy)
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy
+    );
 
   return (
     <table {...getTableProps()}>
@@ -19,8 +17,12 @@ export const Table = ({ columns, data }) => {
         {headerGroups.map((headerGroup, i) => (
           <tr key={i} {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column, i) => (
-              <th key={i} className="sticky top-0 z-40 px-2 py-1 text-sm font-medium leading-4 tracking-wider text-left text-gray-500 border-b border-gray-200 bg-gray-50" {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render('Header')}
+              <th
+                key={i}
+                className="sticky top-0 z-40 px-2 py-1 text-sm font-medium leading-4 tracking-wider text-left text-white border-b border-gray-200 bg-gray-700 hover:bg-gray-500 "
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+              >
+                {column.render("Header")}
               </th>
             ))}
           </tr>
@@ -28,18 +30,24 @@ export const Table = ({ columns, data }) => {
       </thead>
       <tbody className="bg-white" {...getTableBodyProps()}>
         {rows.map((row, i) => {
-          prepareRow(row)
+          prepareRow(row);
           return (
             <tr key={i} {...row.getRowProps()}>
               {row.cells.map((cell, i) => {
-                return <td key={i} className="px-6 py-4 border-b border-gray-200" {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                return (
+                  <td
+                    key={i}
+                    className="px-6 py-4 border-b border-gray-200"
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
               })}
             </tr>
-          )
+          );
         })}
       </tbody>
     </table>
-  )
-}
-
-
+  );
+};
