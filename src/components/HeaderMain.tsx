f71547cc674c07d824ed069a82733944f013e954
higashionna <hihigashi001@gmial.com>
2022-02-09 19:09:40 +0900

    header高さ修正

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -3,11 +3,10 @@ import Link from "next/link";
 
 export const HeaderMain = () => {
   return (
-    <header className="flex　h-14 bg-white">
+    <header className="h-12 bg-white">
       <Link href={"/"}>
         <a className="px-6 py-4 bg-gray-500 text-white mr-4">Menu</a>
       </Link>
-      {/* <button className="px-2 py-4 bg-red-500 text-white" onClick={() =>location.reload()}>データ再取得</button> */}
     </header>
   );
 };
