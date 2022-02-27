b9832fa3d1e8cb229de6c883a8c71bc5669ace31
higashionna <hihigashi001@gmial.com>
2022-02-27 14:35:32 +0900

    tableにデータがない時にメッセージ追加

--- src/components/Table/TableBody.tsx
+++ src/components/Table/TableBody.tsx
@@ -11,12 +11,29 @@ export const TableBody = <T extends object>({
 
   return (
     <tbody {...getTableBodyProps()}>
+      {!rows.length && (
+        <tr>
+          <td
+            className="px-4 py-12 border-b border-gray-200 text-gray-700"
+            colSpan={6}
+          >
+            ※ データがありません
+          </td>
+        </tr>
+      )}
       {rows.map((row) => {
         prepareRow(row);
         return (
           <tr {...row.getRowProps()}>
             {row.cells.map((cell) => {
-              return <td className="px-4 py-2 border-b border-gray-200 text-gray-700" {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+              return (
+                <td
+                  className="px-4 py-2 border-b border-gray-200 text-gray-700"
+                  {...cell.getCellProps()}
+                >
+                  {cell.render("Cell")}
+                </td>
+              );
             })}
           </tr>
         );
